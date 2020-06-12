9bc54fb0c2696a0b44dafe6ef8d3fab26f236c0b
shekhutsav1962001 <shekhutsav1962001@gmail.com>
2020-06-13 00:00:00 +0530

    2020-06-13T00:00:00+05:30

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const jsonfile = require('jsonfile')
-const moment = require('moment')
-const simpleGit = require('simple-git')
-const git = simpleGit()
-const FILEPATH = './data.json'
-
+const jsonfile = require("jsonfile");
+const moment = require("moment");
+const simpleGit = require("simple-git");
+const git = simpleGit();
+const FILEPATH = "./data.json";
 
 // const DATE = moment().subtract(1,'y').format()
-const DATE = moment().format()
+// const DATE = moment().format()
+
+
+const DATE = moment("13.6.2020", "DD.MM.YYYY").format();
+console.log(DATE)
 const data = {
-    date:DATE
-}
+  date: DATE,
+};
 
-
-jsonfile.writeFile(FILEPATH,data,()=>{
-    // callback
-    git.add(['.']).commit(DATE,{'--date':DATE}).push()
-})
-
-
+jsonfile.writeFile(FILEPATH, data, () => {
+  // callback
+  git.add(["."]).commit(DATE, { "--date": DATE }).push();
+});

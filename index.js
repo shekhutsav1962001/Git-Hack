8f9fb7a889606b809e9a561f2966221e932e8226
shekhutsav1962001 <shekhutsav1962001@gmail.com>
2020-06-13 00:00:00 +0530

    2021-06-07T22:11:17+05:30

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ const git = simpleGit();
 const FILEPATH = "./data.json";
 
 // const DATE = moment().subtract(1,'y').format()
-const DATE = moment().format()
+const DATE = moment().format();
 
-
-// const DATE = moment("13.6.2020", "DD.MM.YYYY").format();
+const COMMITDATE = moment("13.6.2020", "DD.MM.YYYY").format();
 const data = {
   date: DATE,
 };
 
 jsonfile.writeFile(FILEPATH, data, () => {
   // callback
-  git.add(["."]).commit(DATE, { "--date": DATE }).push();
+  git.add(["."]).commit(DATE, { "--date": COMMITDATE }).push();
 });

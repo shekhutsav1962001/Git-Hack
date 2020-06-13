74a51fc614ae749876f703ce054d72ec17f53f65
shekhutsav1962001 <shekhutsav1962001@gmail.com>
2020-06-14 00:00:00 +0530

    2021-06-07T22:18:41+05:30

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const FILEPATH = "./data.json";
 
 // const DATE = moment().subtract(1,'y').format()
 const DATE = moment().format();
-const COMMITDATE = moment("13.6.2020", "DD.MM.YYYY").format();
+const COMMITDATE = moment("14.6.2020", "DD.MM.YYYY").format();
 
 const data = {
   date: DATE,
